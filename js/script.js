9c76ac9d43325b7b883e72438804df0cb378303f
D1man4ig <dimakostuk1@gmail.com>
2022-08-16 23:29:07 +0300

    site

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// new Swiper('.image-slider');
+// $('.header__burger').click(function(event){
+//    $('.header__burger,.header_menu').toggleClass('active')
+// });
+// var menu = ['Slide 1', 'Slide 2', 'Slide 3']
+// var mySwiper = new Swiper ('.swiper-container', {
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+// 			clickable: true,
+//         renderBullet: function (index, className) {
+//           return '<span class="' + className + '">' + (menu[index]) + '</span>';
+//         },
+//     },
+
+//     // Navigation arrows
+  
+//   })
